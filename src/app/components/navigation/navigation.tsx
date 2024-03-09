f0e89cc30a65f9796cad6f5585e0eb9b4efa9ec2
KiraTeresa <kira.soba@gmx.de>
2024-03-09 17:19:19 +0100

    Add navigation component and profile page

---
+++ src/app/components/navigation/navigation.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+const Navigation = () => {
+	return (
+		<nav>
+			<ul>
+				<li>HOME</li>
+				<li>PROFILE</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navigation;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+	return <div>This is the profile</div>
+}
+
+export default Profile;
