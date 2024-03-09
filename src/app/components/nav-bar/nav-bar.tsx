4806bca0bd60aa23190dc94d87c247fa0d420608
KiraTeresa <kira.soba@gmx.de>
2024-03-09 18:45:18 +0100

    add links to navBar li elements

--- src/app/components/nav-bar/nav-bar.tsx
+++ src/app/components/nav-bar/nav-bar.tsx
@@ -1,12 +1,13 @@
 'use client'
 import styles from './nav-bar.module.scss';
+import Link from "next/link";
 
 const NavBar = () => {
 	return (
 		<nav className={styles.navBar}>
 			<ul>
-				<li>HOME</li>
-				<li>PROFILE</li>
+				<li><Link href="/">HOME</Link></li>
+				<li><Link href="/profile">PROFILE</Link></li>
 			</ul>
 		</nav>
 	)
